0fd38fd60a7ae4221405751540b8b9a6229afc51
daus89 <diouz89@gmail.com>
2023-10-29 21:41:08 +0800

    odd number

--- oddoreven.py
+++ oddoreven.py
@@ -1,10 +1,9 @@
-# Which number do you want to check?
+#Which number do you want to check?
 number = int(input())
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
-a = number % 2
-if a == 0:
-  print ("This is even number")
+if number % 2 == 0:
+  print ("This is an even number")
 else:
-  print ("This is odd number")
+  print ("This is an odd number"

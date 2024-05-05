7e978ce2225d3ab8e202e250ee597f32c29afd9c
daus89 <diouz89@gmail.com>
2024-05-05 09:06:32 +0800

    day3

---
+++ Day3/pizzaorder.py
@@ -0,0 +1,24 @@
+print("Welcome to my pizza shop!")
+size = input()
+add_pepperoni = input()
+extra_cheese = input()
+
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
